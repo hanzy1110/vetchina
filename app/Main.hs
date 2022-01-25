127e2eac6da26433afc4e429b49678941c9a9b4c
hanzy1110 <eloser1994@gmail.com>
2022-01-25 16:13:43 -0300

    second_Commit

--- app/Main.hs
+++ app/Main.hs
@@ -5,15 +5,35 @@ import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import Data.Foldable
 import Data.Maybe
+import Data.Char
+import Data.List
+import Data.Function
 import System.Directory
+-- import Control.Monad
+import Text.Printf
 
-newtype BoW = BoW {bowToMap:: M.Map T.Text Int} deriving (Show, Read)
+newtype Word' = Word' T.Text deriving (Show,Read,Ord)
+
+mkWord:: T.Text -> Word
+mkWord = Word' . T.toUpper
+
+newtype BoW = BoW 
+    {bowToMap:: M.Map Word Int} deriving (Show, Read)
+
+summaryBoW::BoW -> IO ()
+summaryBoW (BoW bow)= do
+    forM_ (sortBy (compare `on` snd) $ M.toList bow) $ \(w,f) -> printf "%s -> %d \n" w f
+
+
+normalizeTextToWords:: T.Text -> [Word']
+normalizeTextToWords = map mkWord . T.words . T.map (\x -> if isAlphaNum x then x else ' ')
+
 wordtoBoW :: T.Text -> BoW
 wordtoBoW w = BoW $ M.fromList [(w,1)]
 emptyBoW = BoW M.empty
 
 textToBoW :: T.Text -> BoW
-textToBoW = foldMap wordtoBoW . T.words 
+textToBoW = foldMap wordtoBoW . normalizeTextToWords 
 
 --Union with numbers is sum, with strings concatenate
 instance Semigroup BoW where
@@ -35,7 +55,7 @@ wordProbability w bow = fromIntegral n/ (fromIntegral $ wordsCount bow) where
 bowfromFile :: FilePath -> IO BoW
 bowfromFile filepath =
      textToBoW <$> T.readFile filepath
-
+--listDirectory returns [filenames] !!
 bowFromFolder :: FilePath -> IO BoW
 bowFromFolder folderPath = do
     filenames <- listDirectory folderPath
@@ -43,11 +63,25 @@ bowFromFolder folderPath = do
     return $ fold bows
 
 spamBow :: IO BoW
-spamBow = bowFromFolder "app\\data\\spam\\"
+spamBow = bowFromFolder "app\\data\\train\\spam\\"
 
 hamBow :: IO BoW
-hamBow = bowFromFolder "app\\data\\ham\\"
+hamBow = bowFromFolder "app\\data\\train\\ham\\"
 
+wordProbabilitySpam::T.Text->IO Float
+wordProbabilitySpam w = do         
+    pws <- wordProbability w <$> spamBow
+    phs <- wordProbability w <$> hamBow
+    let ps = pws + phs
+    return $ if ps == 0 then 0.0 else pws/ps
+
+textProbabilitySpam::T.Text->IO Float
+textProbabilitySpam text=do
+    let ws = normalizeTextToWords text
+    ps <- mapM wordProbabilitySpam ws
+    let ips = map (\p -> 1.0-p) ps
+    let pp = product ps
+    return (pp/(pp + product ips))
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
