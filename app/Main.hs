306627dead040110130927b50215fbff1c972583
hanzy1110 <eloser1994@gmail.com>
2022-01-26 14:32:20 -0300

    second

--- app/Main.hs
+++ app/Main.hs
@@ -12,23 +12,26 @@ import System.Directory
 -- import Control.Monad
 import Text.Printf
 
-newtype Word' = Word' T.Text deriving (Show,Read,Ord)
+newtype Word' = Word' T.Text deriving (Show,Read,Eq,Ord)
 
-mkWord:: T.Text -> Word
+mkWord:: T.Text -> Word'
 mkWord = Word' . T.toUpper
 
+wordToText:: Word'->T.Text
+wordToText (Word' t) = t
+
 newtype BoW = BoW 
-    {bowToMap:: M.Map Word Int} deriving (Show, Read)
+    {bowToMap:: M.Map Word' Int} deriving (Show, Read)
 
 summaryBoW::BoW -> IO ()
 summaryBoW (BoW bow)= do
-    forM_ (sortBy (compare `on` snd) $ M.toList bow) $ \(w,f) -> printf "%s -> %d \n" w f
+    forM_ (sortBy (compare `on` snd) $ M.toList bow) $ \(w,f) -> printf "%s -> %d \n" (wordToText w) f
 
 
 normalizeTextToWords:: T.Text -> [Word']
 normalizeTextToWords = map mkWord . T.words . T.map (\x -> if isAlphaNum x then x else ' ')
 
-wordtoBoW :: T.Text -> BoW
+wordtoBoW :: Word' -> BoW
 wordtoBoW w = BoW $ M.fromList [(w,1)]
 emptyBoW = BoW M.empty
 
@@ -47,8 +50,8 @@ wordsCount :: BoW -> Int
 wordsCount (BoW bow) = sum $ map snd $ M.toList bow
 
 --M.lookup returns a Maybe!!
-wordProbability :: T.Text -> BoW -> Float
-wordProbability w bow = fromIntegral n/ (fromIntegral $ wordsCount bow) where
+wordProbability ::  BoW -> Word' -> Float
+wordProbability bow w = fromIntegral n/ (fromIntegral $ wordsCount bow) where
     n = fromMaybe 0 $ M.lookup w $ bowToMap bow
 
 -- <$> functor operator, gives me the thing inside the Monad!s
@@ -62,26 +65,40 @@ bowFromFolder folderPath = do
     bows <- mapM (\filename -> bowfromFile (folderPath <> filename)) filenames
     return $ fold bows
 
-spamBow :: IO BoW
-spamBow = bowFromFolder "app\\data\\train\\spam\\"
+data SpamModel = SpamModel {spamBow:: BoW,
+                            hamBow::BoW}
 
-hamBow :: IO BoW
-hamBow = bowFromFolder "app\\data\\train\\ham\\"
+spamModel :: IO SpamModel
+spamModel = do
+    spam<-bowFromFolder "app\\data\\train\\spam\\"
+    ham <-bowFromFolder "app\\data\\train\\ham\\"
+    return $ SpamModel spam ham
 
-wordProbabilitySpam::T.Text->IO Float
-wordProbabilitySpam w = do         
-    pws <- wordProbability w <$> spamBow
-    phs <- wordProbability w <$> hamBow
-    let ps = pws + phs
-    return $ if ps == 0 then 0.0 else pws/ps
+seenWord::  Word' -> SpamModel -> Bool
+seenWord w (SpamModel spamBow hamBow) = isJust sm || isJust hm 
+    where
+     sm = M.lookup w $ bowToMap spamBow
+     hm = M.lookup w $ bowToMap hamBow
+    
+    
+wordProbabilitySpam:: SpamModel -> Word' -> Maybe Float
+wordProbabilitySpam sm@(SpamModel spamBow hamBow) w 
+    | seenWord w sm = 
+        let pws = wordProbability spamBow w 
+            phs = wordProbability hamBow w 
+            ps = pws + phs
+        in Just (pws/ps)
 
-textProbabilitySpam::T.Text->IO Float
-textProbabilitySpam text=do
-    let ws = normalizeTextToWords text
-    ps <- mapM wordProbabilitySpam ws
-    let ips = map (\p -> 1.0-p) ps
-    let pp = product ps
-    return (pp/(pp + product ips))
+    | otherwise = Nothing
+
+textProbabilitySpam::SpamModel -> T.Text -> Float
+textProbabilitySpam spammodel text = 
+    let
+     ws = normalizeTextToWords text
+     ps = mapMaybe (wordProbabilitySpam spammodel) ws
+     ips = map (\p -> 1.0-p) ps
+     pp = product ps
+    in pp/(pp + product ips)
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
